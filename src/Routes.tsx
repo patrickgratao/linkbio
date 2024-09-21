d444315862f44284b1ddde36aa89e2fe8dc4b1c9
Patrick Gratão <ppgratao@gmail.com>
2024-09-21 00:01:54 -0300

    fix: remove unused deps

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,11 @@
 import { Route, Switch } from "react-router-dom";
 
-import { NotFound } from "components/NotFound";
 import { HomePage } from "pages/Home";
 
 export default function AppRoutes() {
 	return (
 		<Switch>
 			<Route key={0} path="/" component={HomePage} exact />
-
-			<Route key={11} path="" component={NotFound} />
 		</Switch>
 	);
 }

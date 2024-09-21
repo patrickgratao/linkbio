fb6d668278413cfdf38bcef56ca42dff453a172e
Patrick Gratão <ppgratao@gmail.com>
2024-09-20 22:08:37 -0300

    chore: adjusments on configs

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,11 +4,11 @@ import { NotFound } from "components/NotFound";
 import { HomePage } from "pages/Home";
 
 export default function AppRoutes() {
-  return (
-    <Switch>
-      <Route key={0} path="/" component={HomePage} exact />
+	return (
+		<Switch>
+			<Route key={0} path="/" component={HomePage} exact />
 
-      <Route key={11} path="" component={NotFound} />
-    </Switch>
-  );
+			<Route key={11} path="" component={NotFound} />
+		</Switch>
+	);
 }
